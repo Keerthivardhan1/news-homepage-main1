92e449e326f6d4976f946be59017f58d71aba3e2
keerthivardhan1 <keerthivardhantekulapelli@gmail.com>
2022-12-25 23:59:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let togglebtn = document.getElementById("togglebtn");
+let menu = document.getElementById("menu");
+
+togglebtn.addEventListener('click' , ()=>{
+    console.log("clicked")
+    menu.style.display = "block";
+    
+})
+
+// window.onload.console.log("loaded")
