d8ded7d4ccf8c7c21d4d3de1bfcfff5429180aaf
keerthivardhan1 <keerthivardhantekulapelli@gmail.com>
2023-02-17 23:23:44 +0530

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 let togglebtn = document.getElementById("togglebtn");
 let menu = document.getElementById("menu");
+let menuitems = document.querySelector(".menuitems");
+let togglebtnimg = document.querySelector("togglebtnimg")
+
+// menuitems == {home , new , popular , trnding , categories}
 
 togglebtn.addEventListener('click' , ()=>{
-    console.log("clicked")
-    menu.style.display = "block";
+    if(menuitems.style.display != "none"){
+        togglebtn.style.backgroundImage = "url('icon-menu.svg')";
+        togglebtn.style.right =0;
+        menuitems.style.display = "none";
+        menu.style.background = "none";
+    }else{
+        menuitems.style.display = "block";
+        menu.style.background = "white";
+        menu.style.opacity = 0.8;
+        menuitems.style.color = "black";
+        menu.style.width = "70vw";
+        menu.style.height = "100%";
+        togglebtn.style.backgroundImage = "url('icon-menu-close.svg')"
+    }
     
 })
-
-// window.onload.console.log("loaded")
